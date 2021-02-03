597bc267317ff53fac0a219718c058b7a57ee457
chris Stellato <stellatocjs@gmail.com>
2021-02-02 17:59:13 -0700

    working version

--- polynomial.py
+++ polynomial.py
@@ -6,17 +6,26 @@ class Polynomial():
         return f"Polynomial({self.coeff_lst})"
 
     def __add__(self, sec_pol):
-        
-        
+        return_lst = []
+        for i, coeff in enumerate(self.coeff_lst):
+            return_lst.append(self.coeff_lst[i] + sec_pol.coeff_lst[i])
+
+        return return_lst
 
     def __sub__(self, sec_pol):
-        for deg, coeff in enumerate(self.coeff_lst):
-            self.coeff_lst[deg]-=sec_pol.coeff_lst[deg]
+        return_lst = []
+        for i, coeff in enumerate(self.coeff_lst):
+            return_lst.append(self.coeff_lst[i] - sec_pol.coeff_lst[i])
+
+        return return_lst
 
 #   How is this line colloquially invoked?
     def __neg__(self):
-        for deg, coeff in enumerate(self.coeff_lst):
-            self.coeff_lst[deg] *= -1
+        return_lst = []
+        for i, coeff in enumerate(self.coeff_lst):
+            return_lst.append(self.coeff_lst[i] * -1)
+
+        return return_lst
 
     def evaluate(self, x):
         total = 0
@@ -24,14 +33,52 @@ class Polynomial():
             total += coeff*x**deg
         return total
 
-test = Polynomial([3,2,1])
-print(test)
-test + Polynomial([3,2,1])
-print(test)
-test - Polynomial([3,2,1])
-print(test)
-# works
-test.__neg__()
-print(test)
-# doesn't work?
-print(-test)
+
+
+    def __eq__(self, sec_poll):
+
+        work_poly1 = self.coeff_lst
+        work_poly2 = sec_poll.coeff_lst
+
+        while work_poly1[-1] == 0:
+            work_poly1.pop()
+
+
+        while work_poly2[-1] == 0:
+            work_poly2.pop()
+
+        return work_poly1 == work_poly2
+
+
+    def __str__(self):
+
+        list_of_strings = []
+
+        for i, val in enumerate(self.coeff_lst):
+            if i == 0:
+                if val > 0:
+                    list_of_strings.append(f'+ {val}')
+                elif val < 0:
+                    list_of_strings.append(f'- {-val}')
+            elif i == 1:
+                if val > 0:
+                    list_of_strings.append(f'+ {val}x')
+                if val < 0:
+                    list_of_strings.append(f'- {-val}x')
+            else:
+                if val > 0:
+                    list_of_strings.append(f'+ {val}x^{i}')
+                elif val < 0:
+                    list_of_strings.append(f'- {-val}x^{i}')
+
+        list_of_strings.reverse()
+
+        print (list_of_strings[0])
+        list_of_strings[0] = list_of_strings[0].replace("- ", "-")
+
+        return ' '.join(list_of_strings)
+        
+        
+
+print((Polynomial([-3,1,0,-2])))
+
